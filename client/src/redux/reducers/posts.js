34b30003bae0d6f9b528ab7441da7afb4ac135d1
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-01-25 21:57:58 +1300

    create redux reducers

---
+++ client/src/redux/reducers/posts.js
@@ -0,0 +1,20 @@
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case "FETCH_POSTS":
+      return action.payload;
+    case "CREATE_POST":
+      return [...state, action.payload];
+    case "UPDATE_POST":
+    case "LIKE_POST":
+      return state.map((p) =>
+        p._id === action.payload._id ? action.payload : p
+      );
+    case "DELETE_POST":
+      return state.filter((p) => p._id !== action.payload);
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
