906f44ee7460bdb3951f4dc207bce2887dac3edd
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-01-28 19:36:00 +1300

    change likeCount to likes in posts model

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -53,14 +53,25 @@ export const deletePost = async (req, res) => {
 
 export const likePost = async (req, res) => {
   const { id } = req.params;
+
+  if (!req.userId) return res.json({ message: "Unauthenticated." });
   if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send("No post found.");
 
   try {
     const post = await PostMessage.findById(id);
+
+    //check if an user has already liked the post
+    const index = post.likes.findIndex((id) => id === String(req.userId));
+    if (index === -1) {
+      post.likes.push(req.userId);
+    } else {
+      post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+
     const result = await PostMessage.findByIdAndUpdate(
       id,
-      { likeCount: post.likeCount + 1 },
+      { likes: post.likes },
       { new: true }
     );
 
