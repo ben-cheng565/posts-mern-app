ad2720cc56fea87c94512d9e6fcc42a6eb3dfe7e
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-01-28 19:22:16 +1300

    create auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const isGoogleAuth = token.length >= 500;
+
+    let decodedData;
+    if (token && !isGoogleAuth) {
+      decodedData = jwt.verify(token, "privatekey");
+
+      req.userId = decodedData.id;
+    } else {
+      decodedData = jwt.decode(token);
+
+      //sub is the identifier of google auth
+      req.userId = decodedData.sub;
+    }
+
+    // after checking the above information, go to next action
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default auth;
